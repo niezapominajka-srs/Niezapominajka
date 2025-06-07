39813ca3a3b9c0361a2ddc01f7755fb89be3682b
mwiktor <malwikt@posteo.com>
2025-06-07 12:18:45 +0200

    -h and --version arg parsing

--- src/niezapominajka/__main__.py
+++ src/niezapominajka/__main__.py
@@ -3,11 +3,30 @@
 # License: GNU GPL version 3 or later
 # Copyright (C) 2025 Wiktor Malinkiewicz
 
-from . import cli
+from sys import argv
 
 
 def main():
-    cli.cli()
+    global mode
+    mode = 'review'
+    argc = len(argv)
+    if argc > 1:
+        match argv[1]:
+            case '--version':
+                mode = None
+                print('Niezapominajka 0.0.1')
+            case '-h' | '--help':
+                mode = None
+                print('''\
+options:
+    --version
+    --help\
+''')
+
+    match mode:
+        case 'review':
+            from . import cli
+            cli.cli()
 
 
 if __name__ == '__main__':
